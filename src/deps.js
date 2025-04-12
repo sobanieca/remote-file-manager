08e0aec0e3639a86862a7c4922f27b4da6bab0e6
Adam Sobaniec <sobanieca@gmail.com>
2025-04-12 19:57:17 +0000

    redundant code removed

--- src/deps.js
+++ src/deps.js
@@ -1,4 +1,2 @@
-//export { assertEquals, assertNotEquals } from "jsr:@std/assert";
-//export { extname, join, basename, dirname } from "jsr:@std/path";
 export { Hono } from "jsr:@hono/hono";
 export { serveStatic } from "jsr:@hono/hono/deno";
