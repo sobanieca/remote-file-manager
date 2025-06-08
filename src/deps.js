8d479551fc7f0ccb29e4b2412f81028abd0dd0d8
Adam Sobaniec <sobanieca@gmail.com>
2025-06-08 20:21:27 +0000

    publish to jsr

--- src/deps.js
+++ src/deps.js
@@ -1,5 +1,5 @@
-export { Hono } from "jsr:@hono/hono";
-export { serveStatic } from "jsr:@hono/hono/deno";
-export { ensureDir, walk } from "jsr:@std/fs";
-export { basename, dirname, extname, join } from "jsr:@std/path";
-export { BlobReader, BlobWriter, ZipWriter } from "jsr:@zip-js/zip-js";
+export { Hono } from "jsr:@hono/hono@4.7.9";
+export { serveStatic } from "jsr:@hono/hono@4.7.9/deno";
+export { ensureDir, walk } from "jsr:@std/fs@1.0.17";
+export { basename, dirname, extname, join } from "jsr:@std/path@1.0.9";
+export { BlobReader, BlobWriter, ZipWriter } from "jsr:@zip-js/zip-js@2.7.62";
