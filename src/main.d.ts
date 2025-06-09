a09a7c9747e82e8b4b3058e393a2f5c2f11e2a30
Adam Sobaniec <sobanieca@gmail.com>
2025-06-09 19:58:52 +0000

    fix: mitigate slow types

---
+++ src/main.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Remote File Manager - A web-based file explorer for managing files on a remote machine.
+ * 
+ * This module starts a Hono.js server that provides:
+ * - File explorer interface
+ * - File upload/download functionality  
+ * - Text file editing capabilities
+ * - Static file serving
+ * 
+ * The server accepts a --port command line argument to specify the port (defaults to 8000).
+ */
+
+/**
+ * No exports - this module starts a server when imported/executed.
+ */
+export {};
